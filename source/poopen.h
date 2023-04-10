edb47cd49f7d54465ce2d0daaa32228e8010cefd
Antonio Niño Díaz <antonio_nd@outlook.com>
2023-04-10 01:44:42 +0100

    licenses: Clarify licensing status
    The COPYING file at the root of the repository is the BSD-3-Clause, but
    the source code files use the ISC license.
    
    This patch adds SPDX license identifiers to all source code files, and
    it also replaces the BSD text of COPYING by the ISC license.

--- source/poopen.h
+++ source/poopen.h
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: ISC
+//
+// Copyright (c) 2008, Mukunda Johnson (mukunda@maxmod.org)
+
 #define slashblash16.it	0
 #define lemon.mod	1
 #define MSL_NSONGS	2
--- source/solution.h
+++ source/solution.h
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: ISC
+//
+// Copyright (c) 2008, Mukunda Johnson (mukunda@maxmod.org)
+
 // MASTER SOLUTION, DEFINITIONS
 #define SFX_CRASH	0
 #define MOD_ACID	0
