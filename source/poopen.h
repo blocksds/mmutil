d26845ac8285068d71dd3efe28ce6c2a468c7b6c
Antonio Niño Díaz <antonio_nd@outlook.com>
2025-07-28 01:56:00 +0100

    chore: Remove unused headers

--- source/poopen.h
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: ISC
-//
-// Copyright (c) 2008, Mukunda Johnson (mukunda@maxmod.org)
-
-#define slashblash16.it	0
-#define lemon.mod	1
-#define MSL_NSONGS	2
-#define MSL_NSAMPS	45
--- source/solution.h
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: ISC
-//
-// Copyright (c) 2008, Mukunda Johnson (mukunda@maxmod.org)
-
-// MASTER SOLUTION, DEFINITIONS
-#define SFX_CRASH	0
-#define MOD_ACID	0
